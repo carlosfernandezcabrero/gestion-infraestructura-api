6f0a44b1e82d747f0527bb2ea4efaf35d6dfbb21
carlosfernandezcabrero <carlitostrabajo2018@outlook.es>
2021-01-18 19:47:31 +0100

    New model GrupoResolutor

---
+++ src/main/java/com/udemy/gestioninfraestructuraapi/model/GrupoResolutor.java
@@ -0,0 +1,24 @@
+package com.udemy.gestioninfraestructuraapi.model;
+
+public class GrupoResolutor {
+
+    private String nombre;
+    private String descripcion;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+}
